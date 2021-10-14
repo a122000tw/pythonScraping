56e084c6873c677d10e3ec64a6183b0f6a754896
a122000tw <a122000tw@gmail.com>
2021-10-14 13:37:56 +0800

    practice

--- scrapingCSS.py
+++ scrapingCSS.py
@@ -5,5 +5,12 @@ html = urlopen("https://mofanpy.com/static/scraping/list.html").read().decode('u
 # print(html)
 soup = BeautifulSoup(html, "lxml")
 month = soup.find_all('li', {'class': 'month'})
-for m in month:
-    print(m.get_text())
+# for m in month:
+    # print(m.get_text())
+    # print(m)
+jan = soup.find('ul', {'class': 'jan'})
+print(jan)
+d_jan = jan.find_all('li')
+
+for d in d_jan:
+    print(d.get_text())
