f5684234f5f9dd97d0ab35dd51bf59c43edc1fd9
a122000tw <a122000tw@gmail.com>
2021-10-14 13:10:47 +0800

    practice

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html = urlopen("https://mofanpy.com/static/scraping/basic-structure.html"
+               ).read().decode('utf-8')
+print(html)
+
+soup = BeautifulSoup(html, features="lxml")
+print(soup.h1)
+print('\n', soup.p)
+all_href = soup.find_all('a')
+for l in all_href:
+    print("href = ", l['href'])
 
